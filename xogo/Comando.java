2553f2ce5bad6058ad36c3260bcf2eef7caf57cc
Antón <32980085+antongomez@users.noreply.github.com>
2019-12-06 21:21:19 +0100

    Menu completado. Falta testear

--- xogo/Comando.java
+++ xogo/Comando.java
@@ -18,17 +18,21 @@ public interface Comando {
     //public void estatisticas();
     //public void hipotecar(Propiedade propiedade, Xogador xogador);
     //public void deshipotecar(Propiedade propiedade);
-    public void lanzarDados(HashMap<String, Dado> dados);
+    public void lanzarDados();
 
     public void listar(String comando1, String grupo);
 
     public boolean rematarPartida();
 
-    //public void rematarTurno();
+    public void rematarTurno();
+
     //public void retroceder();
-    //public void sairCarcere();
+    public void sairCarcere();
+
     //public void vender();
     public void verTaboleiro();
+
+    public void xogador();
     //public void Teletransport();
 
 }
