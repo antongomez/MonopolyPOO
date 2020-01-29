c8f60e65dc2805c1b4193635ca84ca1041df9fc9
Antón <32980085+antongomez@users.noreply.github.com>
2020-01-29 19:19:22 +0100

    Panel esquerdo

---
+++ InterfazGrafica/PanelEsquerdo.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InterfazGrafica;
+
+import estrutura.Taboleiro;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Anton
+ */
+public class PanelEsquerdo extends JPanel {
+
+    private InterfazGrafica ventaPrincipal;
+    private Taboleiro taboleiro;
+    private PanelComandos panelComandos;
+
+    private JTextField textoPe;
+
+    public PanelEsquerdo(InterfazGrafica ventaPrincipal) {
+        this.ventaPrincipal = ventaPrincipal;
+        this.taboleiro = taboleiro;
+        this.panelComandos = new PanelComandos(ventaPrincipal);
+
+        this.textoPe = new JTextField("Ningún tipo seleccionado");
+
+        this.textoPe.setEnabled(false);
+        this.textoPe.setDisabledTextColor(Color.BLUE);
+        this.textoPe.setBackground(this.getBackground());
+        this.textoPe.setBorder(null);
+
+        BorderLayout layout = new BorderLayout();
+        this.setLayout(layout);
+
+        this.add(panelComandos, BorderLayout.NORTH);
+        this.add(this.textoPe, BorderLayout.SOUTH);
+
+    }
+
+    public InterfazGrafica getVentaPrincipal() {
+        return ventaPrincipal;
+    }
+
+    public void setVentaPrincipal(InterfazGrafica ventaPrincipal) {
+        this.ventaPrincipal = ventaPrincipal;
+    }
+
+    public Taboleiro getTaboleiro() {
+        return taboleiro;
+    }
+
+    public void setTaboleiro(Taboleiro taboleiro) {
+        this.taboleiro = taboleiro;
+    }
+
+    public PanelComandos getPanelComandos() {
+        return panelComandos;
+    }
+
+    public void setPanelComandos(PanelComandos panelComandos) {
+        this.panelComandos = panelComandos;
+    }
+
+    public JTextField getTextoPe() {
+        return textoPe;
+    }
+}
