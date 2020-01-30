9593d75d58b8ef1940056695fde781273bf78565
Antón <32980085+antongomez@users.noreply.github.com>
2020-01-30 13:43:24 +0100

    Describir casilla

--- InterfazGrafica/DescripVenta.java
+++ InterfazGrafica/DescripVenta.java
@@ -4,6 +4,7 @@ import estrutura.Casilla;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class DescripVenta extends JDialog {
 
@@ -23,8 +24,11 @@ public class DescripVenta extends JDialog {
         int width = pantalla.width;
 
         //Configuramos a venta
-        setLocation(width / 4, height / 4);
-        this.setSize((int) Math.ceil(width / 1.75), (int) Math.ceil(height / 1.5));
+        this.setSize(920, 480);
+        this.setBackground(Color.BLUE);
+        int localizacionAncho = (int) Math.round(width / 2) - (int) Math.round(getSize().getWidth() / 2);
+        int localizacionAlto = (int) Math.round(height / 2) - (int) Math.round(getSize().getHeight() / 2);
+        this.setLocation(localizacionAncho, localizacionAlto);
         this.setResizable(false); //Non redimensionable
         this.setTitle(" ");
         this.setVisible(true);
@@ -33,43 +37,54 @@ public class DescripVenta extends JDialog {
         this.panel = new JPanel();
         this.panel.setLayout(new BorderLayout());
         this.panel.setVisible(true);
+        this.panel.setOpaque(false);
         this.add(panel);
 
         //Construimos paneles auxiliares
         this.leste = new JPanel();
-        this.leste.setPreferredSize(new Dimension(width / 4, (int) Math.ceil(height / 1.5)));
-        this.leste.setSize(new Dimension(width / 4, (int) Math.ceil(height / 1.5)));
+        this.leste.setOpaque(false);
+        this.leste.setPreferredSize(new Dimension(getWidth() / 2, getHeight()));
+        this.leste.setSize(leste.getPreferredSize());
         this.panel.add(leste, BorderLayout.WEST);
         this.leste.setLayout(new BorderLayout());
-        this.leste.setBackground(Color.BLUE);
         this.leste.setOpaque(true);
         this.este = new JPanel();
-        this.este.setPreferredSize(new Dimension(width / 3, (int) Math.ceil(height / 1.5)));
+        this.este.setOpaque(false);
+        this.este.setPreferredSize(new Dimension(getWidth() / 2, getHeight()));
         this.panel.add(este, BorderLayout.EAST);
         this.este.setLayout(new BorderLayout());
 
-        //Introuducimos Jbutton
-        this.imaxe = new JButton();
-        this.imaxe.setPreferredSize(new Dimension(getWidth() - 40, getHeight() - 40));
-        this.imaxe.setSize(new Dimension(getWidth() - 40, getHeight() - 40));
-        this.imaxe.setEnabled(true);
-        this.leste.add(imaxe, BorderLayout.CENTER);
-
+        //Introducimos os espazos adicionais para o JButton
         JPanel panelNorte = new JPanel();
-        panelNorte.setPreferredSize(new Dimension(width / 4, 20));
+        panelNorte.setPreferredSize(new Dimension(getWidth() / 2, 30));
         this.leste.add(panelNorte, BorderLayout.NORTH);
 
         JPanel panelSur = new JPanel();
-        panelSur.setPreferredSize(new Dimension(width / 4, 20));
-        this.leste.add(panelNorte, BorderLayout.SOUTH);
+        panelSur.setPreferredSize(new Dimension(getWidth() / 2, 30));
+        this.leste.add(panelSur, BorderLayout.SOUTH);
 
         JPanel panelOeste = new JPanel();
-        panelOeste.setPreferredSize(new Dimension(20, (int) Math.ceil(height / 1.5)));
-        this.leste.add(panelNorte, BorderLayout.WEST);
+        panelOeste.setPreferredSize(new Dimension(50, leste.getHeight()));
+        panelOeste.setOpaque(false);
+        this.leste.add(panelOeste, BorderLayout.WEST);
 
-        JPanel PanelEste = new JPanel();
-        PanelEste.setPreferredSize(new Dimension(20, (int) Math.ceil(height / 1.5)));
-        this.leste.add(panelNorte, BorderLayout.EAST);
+        JPanel panelEste = new JPanel();
+        panelEste.setPreferredSize(new Dimension(50, leste.getHeight()));
+        this.leste.add(panelEste, BorderLayout.EAST);
+
+        //Introuducimos Jbutton
+        JPanel panelImaxe = new JPanel();
+        int ancho = 360;
+        int alto = 420;
+        panelImaxe.setPreferredSize(new Dimension(ancho, alto));
+        panelImaxe.setSize(panelImaxe.getPreferredSize());
+        //FlowLayout layout = new FlowLayout(FlowLayout.CENTER);
+        this.imaxe = new JButton();
+        this.imaxe.setPreferredSize(panelImaxe.getPreferredSize());
+        this.imaxe.setSize(imaxe.getPreferredSize());
+        this.imaxe.setEnabled(true);
+        panelImaxe.add(imaxe);
+        this.leste.add(panelImaxe, BorderLayout.CENTER);
 
         //Texto
         this.info = new JTextArea();
@@ -84,7 +99,7 @@ public class DescripVenta extends JDialog {
         this.aux.setPreferredSize(new Dimension(width / 4, (int) Math.ceil(height / 12)));
         this.este.add(aux, BorderLayout.NORTH);
 
-        //FUnción para engadir informacion
+        //Función para engadir informacion
         this.inciarDescripVenta(casilla);
     }
 
