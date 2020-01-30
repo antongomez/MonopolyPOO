f39fc20568bd3d150bc9eb1c3a06b568e7d52e65
victorxbd <victorxesus.barreiro@rai.usc.es>
2020-01-30 18:52:45 +0100

    asdfasdfas

--- InterfazGrafica/PanelSaida.java
+++ InterfazGrafica/PanelSaida.java
@@ -1,7 +1,25 @@
 package InterfazGrafica;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.security.Principal;
 
 public class PanelSaida extends JPanel {
+    private JPanel principal;
+    private JTextArea terminal;
+
+    public PanelSaida(Dimension dimension)
+    {
+        principal = new JPanel();
+        principal.setPreferredSize(dimension);
+        principal.setOpaque(false);
+        principal.setVisible(true);
+
+
+        terminal = new JTextArea();
+        terminal.setForeground(Color.GREEN);
+        terminal.setBackground(Color.BLACK);
+        terminal.setVisible(true);
+    }
 
 }
